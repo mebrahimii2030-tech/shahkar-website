e34849016be206f71bb5020c4e518baeb04784f4
M Ebrahimi <m.ebrahimii2030@gmail.com>
2026-07-04 16:45:19 +0330

    Update website

--- js/main.js
+++ js/main.js
@@ -186,3 +186,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+// ==============================
+// MOBILE MENU
+// ==============================
+
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+
+if (menuToggle && nav) {
+  menuToggle.addEventListener("click", function () {
+    nav.classList.toggle("active");
+  });
+}
